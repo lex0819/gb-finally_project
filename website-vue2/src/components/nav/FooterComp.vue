60d65e94e2b47b4c6a22a69671a07aef389686f0
lex0819 <lexmolnar@gmail.com>
2024-04-27 14:08:21 +0300

    seminar05 add components for header and footer

---
+++ website-vue2/src/components/nav/FooterComp.vue
@@ -0,0 +1,143 @@
+<template>
+    <div>
+        <footer class="footer">
+            <div class="wrapper">
+                <div class="footer__inner">
+                    <div class="footer__item">
+                        <a href="./index.html" class="logo">
+                            <LogoComp />
+                        </a>
+                        <p class="footer__text large-paragraph">
+                            It is a long established fact that a reader will be
+                            distracted looking.
+                        </p>
+                        <ul class="footer__social">
+                            <li class="footer__social_item">
+                                <a href="#"><TwitterComp /></a>
+                            </li>
+                            <li class="footer__social_item">
+                                <a href="#"><LinkedIn /></a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="footer__item">
+                        <h3 class="footer__title header3">Pages</h3>
+                        <ul class="footer__pages">
+                            <li class="large-paragraph">
+                                <a href="#">Home</a>
+                            </li>
+                            <li class="large-paragraph">
+                                <a href="#">Project</a>
+                            </li>
+                            <li class="large-paragraph">
+                                <a href="#">Blog</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="footer__item">
+                        <ContactComp />
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+</template>
+
+<script>
+import LogoComp from "@/components/nav/LogoComp.vue";
+import TwitterComp from "@/components/social/TwitterComp.vue";
+import LinkedIn from "@/components/social/LinkedIn.vue";
+import ContactComp from "@/components/nav/ContactComp.vue";
+
+export default {
+    name: 'FooterComp',
+    components:{
+        LogoComp,
+        TwitterComp,
+        LinkedIn,
+        ContactComp,
+    },
+    data() {
+        return {},
+    },
+
+     mounted() {},
+
+    methods: {},
+};
+</script>
+<style lang="scss" scoped>
+.footer {
+    padding-top: 96px;
+    padding-bottom: 134px;
+
+    &__inner {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        grid-template-rows: auto;
+        column-gap: 27px;
+
+        @media (max-width: 600px) {
+            grid-template-columns: 1fr;
+            row-gap: 27px;
+            padding-left: 30px;
+            padding-right: 30px;
+        }
+    }
+
+    &__item {
+        @media (max-width: 600px) {
+            text-align: center;
+        }
+    }
+
+    &__text {
+        margin-top: 18px;
+        white-space: pre-line;
+    }
+
+    &__title {
+        text-align: center;
+    }
+
+    &__social {
+        margin-top: 31px;
+        display: flex;
+        align-items: center;
+        gap: 50px;
+
+        @media (max-width: 600px) {
+            justify-content: center;
+        }
+
+        &_item {
+            width: 20px;
+            height: 20px;
+
+            & > a {
+                display: block;
+                width: 20px;
+                height: 20px;
+            }
+        }
+    }
+
+    &__pages {
+        text-align: center;
+
+        & > li {
+            margin-top: 36px;
+        }
+    }
+
+    &__contact {
+        margin-top: 34px;
+        white-space: pre-line;
+        text-align: right;
+
+        @media (max-width: 600px) {
+            text-align: center;
+        }
+    }
+}
+</style>
---
+++ website-vue2/src/components/nav/HeaderComp.vue
@@ -0,0 +1,72 @@
+<template>
+    <div>
+        <header class="header">
+            <div class="wrapper">
+                <div class="header__inner">
+                    <a href="./index.html" class="logo">
+                        <LogoComp />
+                    </a>
+                    <nav class="menu">
+                        <ul class="menu__list">
+                            <li class="menu__item">
+                                <a href="./index.html" target="_blank">Home</a>
+                            </li>
+                            <li class="menu__item">
+                                <a href="#" target="_blank">Project</a>
+                            </li>
+                            <li class="menu__item">
+                                <a href="./blog.html" target="_blank">Blog</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </header>
+    </div>
+</template>
+
+<script>
+import LogoComp from '@/components/nav/LogoComp.vue';
+
+export default {
+    name: 'HeaderComp',
+    components: {
+        LogoComp,
+    },
+
+    data() {
+        return {},
+    },
+
+    mounted() {},
+
+    methods: {},
+};
+</script>
+<style lang="scss" scoped>
+.header {
+    padding-top: 44px;
+    padding-bottom: 45px;
+
+    &__inner {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        flex-wrap: wrap;
+    }
+}
+
+.menu {
+    &__list {
+        display: flex;
+        justify-content: end;
+        gap: 45px;
+        align-items: center;
+    }
+
+    &__item {
+        font-size: 20px;
+        line-height: 1.25;
+    }
+}
+</style>
